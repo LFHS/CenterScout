719380918f8ffb39ea18a56c2c744d08ef69e88a
Will Franzen <wtfranzen@gmail.com>
2014-02-21 11:28:33 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
 new Konami().load('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+console.log("%c Hey! If you're interested in how the site works, you should check out the source code on GitHub! https://github.com/LFHS/CenterScout/", 'background: #222; color: #bada55');
