46e8064c87ff29fe8c09263b70fb7c32bcca9dd5
Will Franzen <wtfranzen@gmail.com>
2014-02-24 09:28:23 -0600

    Created function to automatically log in a user
    Created function to automatically log in a user to the official https://www.lakeforestschools.org website.
    
    This is the beginning of the LFHS.org API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+var username = "";
+var password = "";
+
 new Konami().load('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 console.log("%c Hey! If you're interested in how the site works, you should check out the source code on GitHub! https://github.com/LFHS/CenterScout/", 'background: #222; color: #bada55');
+
+function loginToLFHS(url) {
+	var form = document.createElement("form");
+	form.setAttribute("method", "POST");
+	form.setAttribute("action", "/login/index.aspx");
+	
+	var loginData = {
+		Email: username, // Why is a student's ID put in a field called Email :v
+		password: password,
+		rememberMe: 'false',
+		LinkID: '',
+		Referrer: '/students/index.aspx', // Why not use the Referrer HTTP header?
+		pageaction: 'login'
+	}
+
+	for(var key in loginData) {
+		if(params.hasOwnProperty(key)) {
+			var hiddenField = document.createElement("input");
+			hiddenField.setAttribute("type", "hidden");
+			hiddenField.setAttribute("name", key);
+			hiddenField.setAttribute("value", params[key]);
+
+			form.appendChild(hiddenField);
+		}
+	}
+
+	document.body.appendChild(form);
+	form.submit();
+}
