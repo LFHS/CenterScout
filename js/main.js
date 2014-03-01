28c1fe6339a4d7b921e92105dc7121015f3c2374
Will Franzen <wtfranzen@gmail.com>
2014-02-28 19:04:19 -0600

    Autoremove hash from URLs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var username = "";
 var password = "";
 
+window.location.hash = "";
+
 console.log("%c Hey! If you're interested in how the site works, you should check out the source code on GitHub! https://github.com/LFHS/CenterScout/", 'background: #222; color: #bada55');
 
 function loginToLFHS(url) {
