eba793e3c20060e4678b71609443aea326d8bb3a
Will Franzen <wtfranzen@gmail.com>
2014-05-04 22:36:24 -0500

    Setup http module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var consolidate = require('consolidate');
+var http = require('http');
 
 var app = express();
 
@@ -10,3 +11,7 @@ app.use(express.static('./public'));
 
 app.engine('hbs', consolidate.handlebars);
 app.set('view engine', 'hbs');
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Server started.');
+});
