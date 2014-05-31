64fe8a46ad41992875bab4146eb2f0233f538e98
Will Franzen <wtfranzen@gmail.com>
2014-05-30 22:59:12 -0500

    Change template URLs to relative

--- web/main.js
+++ web/main.js
@@ -3,27 +3,27 @@ var CenterScout = angular.module('CenterScout', ['ngRoute']);
 CenterScout.config(function($routeProvider) {
     $routeProvider.when('/', {
         controller: 'HomeController',
-        templateUrl: '/views/home.html'
+        templateUrl: 'views/home.html'
     });
 
     $routeProvider.when('/home', {
         controller: 'HomeController',
-        templateUrl: '/views/home.html'
+        templateUrl: 'views/home.html'
     });
 
     $routeProvider.when('/class', {
         controller: 'ClassController',
-        templateUrl: '/views/class.html'
+        templateUrl: 'views/class.html'
     });
 
     $routeProvider.when('/about', {
         controller: 'AboutController',
-        templateUrl: '/views/about.html'
+        templateUrl: 'views/about.html'
     });
 
     $routeProvider.when('/404', {
         controller: 'ErrorController',
-        templateUrl: '/views/404.html'
+        templateUrl: 'views/404.html'
     });
 
     $routeProvider.otherwise({
