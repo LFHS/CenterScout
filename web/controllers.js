e1797a89a51b77f620ea976f2eec00374ccb8dde
Will Franzen <wtfranzen@gmail.com>
2014-05-30 21:41:44 -0500

    Angular template now functional

--- web/controllers.js
+++ web/controllers.js
@@ -1,5 +1,24 @@
 CenterScout.controller('HomeController', ['$scope', function($scope) {
-
+    $scope.assignments = [
+        {
+            'done':  false,
+            'name':  'Read p.325-330',
+            'class': 'WC',
+            'date':  '02/05'
+        },
+        {
+            'done':  false,
+            'name':  'WS p.524#1-10',
+            'class': 'M',
+            'date':  '05/29'
+        },
+        {
+            'done':  true,
+            'name':  'CAD Presentation',
+            'class': 'CAD',
+            'date':  '05/30'
+        },
+    ];
 }]);
 
 CenterScout.controller('ClassController', ['$scope', function($scope) {
