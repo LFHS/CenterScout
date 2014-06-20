2f8dbd3e42ecf304da929a536889398006316d30
Will Franzen <wtfranzen@gmail.com>
2014-06-20 18:15:57 -0500

    Got it mostly working

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,18 +1,27 @@
 CenterScout.controller('HomeController', ['$scope', 'gradeData', 'assignmentData', function($scope, gradeData, assignmentData) {
 
-    $scope.assignments = [{ done: false, name: 'Loading...', class: '', date: ''}]; // TODO: Find a better way of doing this hack
     $scope.grades = [{ name: 'Loading...', class: '', date: '', percent: '', fraction: ''}];
+    $scope.assignments = [{ done: false, name: 'Loading...', class: '', date: ''}]; // TODO: Find a better way of doing this hack
+    $scope.courses = ['M', 'CAD']; // TODO: Find a way to use the PowerSchool names for only the classes in lfhs.or
+    $scope.selectedCourse = '...';
 
-    $scope.classes = []; // TODO: Find a way to use the PowerSchool names for only the classes in lfhs.org
+    $scope.setSelectedCourse = function() {
+        $scope.selectedCourse = this.course;
+    };
 
     gradeData().success(function(grades) {
         $scope.grades = grades;
+
+        grades.forEach(function(grade) {
+            if($scope.courses.indexOf(grade.class) === -1)
+                $scope.courses.push(grade.class);
+        });
+        $scope.selectedCourse = $scope.courses[0];
     });
 
     assignmentData().success(function(assignments) {
         $scope.assignments = assignments;
     });
-
 
 }]);
 
--- web/js/providers.js
+++ web/js/providers.js
@@ -1,11 +1,11 @@
 CenterScout.factory('gradeData', ['$http', function($http) {
     return function() {
-        return $http.get('https://localhost:8080/api/grades');
+        return $http.get('http://localhost:8080/api/grades');
     };
 }]);
 
 CenterScout.factory('assignmentData', ['$http', function($http) {
     return function() {
-        return $http.get('https://localhost:8080/api/assignments');
+        return $http.get('http://localhost:8080/api/assignments');
     };
 }]);
