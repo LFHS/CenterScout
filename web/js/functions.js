524aecebb4a728168e8742035a602a95d3080fc6
Will Franzen <wtfranzen@gmail.com>
2014-06-23 12:09:28 -0500

    Update error handling

--- web/js/functions.js
+++ web/js/functions.js
@@ -2,6 +2,7 @@ function isCordova() {
     return !!window.location.href.match(/cordova=1/);
 }
 
-function handleError() {
-    alert('An Internal Error Has Occured');
+function handleError(error) {
+    // TODO: Show user some kind of error
+    console.log(error);
 }
