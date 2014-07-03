4247c61e8262a637cc165ca28f000719ff624c12
Will Franzen <wtfranzen@gmail.com>
2014-07-02 21:02:52 -0500

    Create yes/no string utility functions

--- web/js/functions.js
+++ web/js/functions.js
@@ -6,3 +6,12 @@ function handleError(error) {
     // TODO: Show user some kind of error
     console.log(error);
 }
+
+// Using yes/no because I don't like storing true/false in strings for localStorage
+function strToBool(string) {
+    return (string == 'yes')? true : false;
+}
+
+function boolToStr(boolean) {
+    return boolean? 'yes' : 'no';
+}
